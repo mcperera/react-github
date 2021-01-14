fcef05898c3e072a69ee2eb044cfe65a50a46f96
Madushan Perera <madushanperera3817@gmail.com>
2021-01-15 05:14:20 +0530

    Add styled components, router-dom, icons

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Info from "./Info/Info";
+import Repos from "./Repos/Repos";
+import User from "./User/User";
+import Search from "./Search/Search";
+import Navbar from "./Navbar/Navbar";
+
+export { Info, Repos, User, Search, Navbar };
