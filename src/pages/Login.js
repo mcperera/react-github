319fe564df51eff571f6af658068bf1fae5d4cc9
Madushan Perera <madushanperera3817@gmail.com>
2021-01-15 06:01:36 +0530

    Set Login and Error page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import loginImg from "../assets/images/login-min.jpg";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <LoginWrapper>
+      <div className="container">
+        <img src={loginImg} alt="login" />
+        <h2>GitHub User</h2>
+        <button className="btn">Login</button>
+      </div>
+    </LoginWrapper>
+  );
 }
+
+const LoginWrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: #29cdc2;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    width: 70%;
+    margin-bottom: 1.5rem;
+  }
+  h2 {
+    margin-bottom: 1.5rem;
+  }
+`;
 
 export default Login;
